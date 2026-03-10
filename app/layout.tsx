d33be6e6fd75d124c5322c82442cf978699f0805
WR Digital <wrdigital@MacBook-Roberto.local>
2026-03-10 12:59:15 +0100

    perf: defer Google Tag Manager using lazyOnload for mobile PageSpeed

--- app/layout.tsx
+++ app/layout.tsx
@@ -140,7 +140,7 @@ export default async function RootLayout({
         </noscript>
         <Script
           id="google-tag-manager"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
