85abfaf87379917b7b0973f438388bb1a6a32687
WR Digital <wrdigital@MacBook-Roberto.local>
2026-06-04 19:37:23 +0200

    feat: add Leadfeeder visitor tracking script
    - Script ID: YEgkB8lbb2w4ep3Z
    - strategy='afterInteractive' per non bloccare il rendering
    - Aggiunto nell'<head> del root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -124,6 +124,14 @@ export default async function RootLayout({
   return (
     <html lang="it" suppressHydrationWarning className={inter.variable} style={{ '--icon-color': config?.iconColor || '#eab308' } as React.CSSProperties}>
       <head>
+        {/* Leadfeeder – company visitor identification */}
+        <Script
+          id="leadfeeder-tracker"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('YEgkB8lbb2w4ep3Z');`,
+          }}
+        />
       </head>
       <body suppressHydrationWarning>
         <ScrollProgress />
